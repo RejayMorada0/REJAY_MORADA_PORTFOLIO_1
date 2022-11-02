eb47c0ab676171aa5952d519a82cbbc25f3ff9e8
RejayMorada0 <92773912+RejayMorada0@users.noreply.github.com>
2022-11-02 09:23:02 +0800

    add animation

--- js/function.js
+++ js/function.js
@@ -1,42 +1,46 @@
-gsap.registerPlugin(ScrollTrigger);
-    // REVEAL //
-    gsap.utils.toArray(".revealUp").forEach(function (elem) {
-      ScrollTrigger.create({
-        trigger: elem,
-        start: "top 80%",
-        end: "bottom 20%",
-        markers: true,
-        onEnter: function () {
-          gsap.fromTo(
-            elem,
-            { y: 100, autoAlpha: 0 },
-            {
-              duration: 1.25,
-              y: 0,
-              autoAlpha: 1,
-              ease: "back",
-              overwrite: "auto"
-            }
-          );
-        },
-        onLeave: function () {
-          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-        },
-        onEnterBack: function () {
-          gsap.fromTo(
-            elem,
-            { y: -100, autoAlpha: 0 },
-            {
-              duration: 1.25,
-              y: 0,
-              autoAlpha: 1,
-              ease: "back",
-              overwrite: "auto"
-            }
-          );
-        },
-        onLeaveBack: function () {
-          gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+var words = ['Making websites that capture users.', 'Planned digital solutions.'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
         }
-      });
-    });
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
